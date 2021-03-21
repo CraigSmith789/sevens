47e37acf34ef45a193a458f1852808e8e43d9aea
CraigSmith789 <craig789@gmail.com>
2021-03-21 18:15:11 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -6,21 +6,24 @@ import GameBoard from './containers/GameBoard'
 import Rules from './components/Rules'
 import Stats from './components/Stats'
 
-
-
 function App () {
   return (
-    
     <Router>
       <div className='App'>
         <NavBar />
-        <header>Welcome to Sevens</header>
+        <header
+          style={{
+            fontSize: '30px',
+            paddingBottom: '15px'
+          }}
+        >
+          <b>Welcome to Sevens</b>
+        </header>
         <Route exact path='/' render={() => <GameBoard />} />
         <Route exact path='/rules' render={() => <Rules />} />
         <Route exact path='/stats' render={() => <Stats />} />
       </div>
     </Router>
-   
   )
 }
 export default App
